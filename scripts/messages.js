7b62f25ca22c192bf6e0c0aabaab67b8a22b7071
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-31 15:02:34 -0500

    again

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,5 +1,5 @@
 /* globals WebSocket, location */
-const HOST = location.origin.href(/^https/, 'wss');
+const HOST = location.origin.replace(/^https/, 'wss');
 console.log(HOST);
 
 const ws = new WebSocket(HOST);
