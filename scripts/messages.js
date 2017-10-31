8903426a4487492db7a01e9e94e9ac390ae52e3e
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-31 14:40:41 -0500

    use full location for path

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,5 +1,5 @@
 /* globals WebSocket, location */
-const HOST = location.origin.replace(/^https/, 'wss');
+const HOST = location.origin.href(/^https/, 'wss');
 console.log(HOST);
 
 const ws = new WebSocket(HOST);
