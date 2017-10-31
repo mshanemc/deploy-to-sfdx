0f275ee09ae37f6fab9deed38a71c38832ac0225
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-31 13:40:59 -0500

    client cleanup

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,5 +1,6 @@
-var HOST = location.origin.replace(/^http/, 'ws')
-var ws = new WebSocket(HOST);
+/* globals WebSocket, location */
+const HOST = location.origin.replace(/^http/, 'ws');
+const ws = new WebSocket(HOST);
 
 ws.onmessage = function (event) {
   console.log(event);
