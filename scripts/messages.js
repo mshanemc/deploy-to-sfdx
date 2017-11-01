13fac3d852bbade336e0f0ea25b9a36df045ab6a
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-10-31 22:43:03 -0500

    front stuff

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,9 +1,17 @@
-/* globals WebSocket, location */
+/* globals WebSocket, location, document */
 const HOST = location.href.replace(/^http/, 'ws');
 console.log(HOST);
 
 const ws = new WebSocket(HOST);
+const deployId = document.getElementById('deployId').value;
+console.log('deployId is ' + deployId);
 
 ws.onmessage = function (event) {
-  console.log(event.data);
+	if (event.data.deployId === deployId){
+		console.log('mine');
+		console.log(event.data);
+	} else {
+		console.log('not mine');
+		console.log(event.data);
+	}
 };
