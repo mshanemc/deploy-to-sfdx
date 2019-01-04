ae8c811a354556bde084bfa016746bdf0b8c0c74
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-04 08:55:20 -0600

    some simple server tests

--- test/helpers/init.ts
+++ test/helpers/init.ts
@@ -4,5 +4,8 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-process.env.DEPLOYER_TESTING_ENDPOINT = 'http://localhost:8443';
+// process.env.DEPLOYER_TESTING_ENDPOINT = 'http://localhost:8443';
+process.env.DEPLOYER_TESTING_ENDPOINT =
+  'https://hosted-scratch-dev.herokuapp.com';
+
 process.env.LOG_LEVEL = 'ERROR';
---
+++ test/integrationTests/miscServerTests.ts
@@ -0,0 +1,32 @@
+import * as chai from 'chai';
+import * as Nightmare from 'nightmare';
+import * as dotenv from 'dotenv';
+import * as request from 'request-promise-native';
+
+dotenv.config();
+
+const expect = chai.expect;
+const testEnv = process.env.DEPLOYER_TESTING_ENDPOINT;
+const waitTimeout = 1000 * 60 * 15; // 15 minutes should do it
+
+it('returns the home page empty message', async () => {
+  const response = await request.get({
+    url: `${testEnv}/`,
+    json: true
+  });
+  expect(response.message).to.equal(
+    'There is nothing at /.  See the docs for valid paths.'
+  );
+});
+
+it('displays pools', async () => {
+  const response = await request.get({
+    url: `${testEnv}/pools`,
+    json: true
+  });
+  expect(response).to.be.an('array');
+});
+
+it('returns the testform');
+
+it('returns the userinfo form');
