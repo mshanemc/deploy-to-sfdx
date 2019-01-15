7660413c914dc8c6a2ce3c277e0582830956cb08
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-15 12:56:30 -0600

    async iife and redis exported

--- src/lib/deployOneOff.ts
+++ src/lib/deployOneOff.ts
@@ -2,11 +2,8 @@ import * as logger from 'heroku-logger';
 import * as checkQueue from './deployQueueCheck';
 import * as hubAuth from './hubAuth';
 
-
-logger.debug('A one-off deploy consumer dyno is up!');
-
-hubAuth()
-  .then(async () => {
-    await checkQueue();
-    process.exit(0);
-  });
+async () => {
+  logger.debug('A one-off deploy consumer dyno is up!');
+  await hubAuth();
+  await checkQueue();
+};
