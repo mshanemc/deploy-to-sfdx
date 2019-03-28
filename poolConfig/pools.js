7c67fc43c4750ba7ae023d585f4ddaf273e57230
jrattanpal <jrattanpal@gmail.com>
2019-03-28 13:25:10 -0400

    Update pools.js

--- poolConfig/pools.js
+++ poolConfig/pools.js
@@ -1,14 +1,14 @@
-module.exports = [
+[
 	{
-		user: 'Vchalem',
-		repo : 'TEFlowsLearning',
-		quantity: 2,
-		lifeHours: 24
+		"user": "Vchalem",
+		"repo" : "TEFlowsLearning",
+		"quantity": 2,
+		"lifeHours": 24
 	},
 	{
-		user: 'rreboucas',
-		repo: 'sparkle-dx',
-		quantity: 2,
-		lifeHours: 24
+		"user": "rreboucas",
+		"repo": "sparkle-dx",
+		"quantity": 2,
+		"lifeHours": 24
 	}
-];
+]
