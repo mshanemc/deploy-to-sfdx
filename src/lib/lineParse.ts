a31e88abee6a7a1671e96a279265844a4c758d67
jrattanpal <jrattanpal@gmail.com>
2019-03-29 10:45:18 -0400

    Update lineParse.ts

--- src/lib/lineParse.ts
+++ src/lib/lineParse.ts
@@ -42,11 +42,9 @@ const lineParse = function(msgJSON: deployRequest): Promise<string[]> {
             // it's an OK line.
             logger.debug('lineParse: line pushed');
 
-            //TEmp fix to force sfdx update
-            if (!line.startsWith('sfdx update')) {
             // json-ify every line
             line = `${argStripper(line, '--json', true)} --json`;
-            }
+            
             parsedLines.push(jsonify(line));
           }
         }
