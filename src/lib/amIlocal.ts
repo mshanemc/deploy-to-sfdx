49ab6c9aea1c2833d640c069dcff1b8c153c9a60
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-01 17:39:48 -0500

    cwd is a function

--- src/lib/amIlocal.ts
+++ src/lib/amIlocal.ts
@@ -1,6 +1,6 @@
 const isLocal = () => {
     // return ! (  fs.existsSync('/app') );
-    console.log(process.cwd);
+    console.log(process.cwd());
     // return process.cwd() === '/app';
     return false;
 };
