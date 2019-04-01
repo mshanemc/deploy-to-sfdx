eb691acfcbe48d9d68eb13b5ae19dfd3caedfb5e
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-01 17:27:54 -0500

    remove me-specific tests

--- test/integrationTests/generatedRepos/mshanemc-cg1-lowcode.test.ts
+++
@@ -1,7 +0,0 @@
-
-import { deployCheck } from './../../helpers/deployCheck';
-import { sfdxTimeout } from './../../helpers/testingUtils';
-
-test('non-pool grab of the org mshanemc/cg1-lowcode', async () => {
-    await deployCheck('mshanemc', 'cg1-lowcode');
-}, sfdxTimeout);     
