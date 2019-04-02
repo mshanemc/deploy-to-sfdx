5c70dd17e91d092581121388d6167bc7c469c0a4
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-01 23:13:32 -0500

    builder as async command

--- src/processes/poolMaintenance.ts
+++ src/processes/poolMaintenance.ts
@@ -1,8 +1,12 @@
 import * as logger from 'heroku-logger';
+import * as util from 'util';
+import { exec } from 'child_process';
 
 import * as utilities from '../lib/utilities';
 import { getPoolDeployRequestQueueSize } from '../lib/redisNormal';
 import { prepareAll } from '../lib/poolPrep';
+
+const execProm = util.promisify(exec);
 
 const maxPoolBuilders = parseInt(process.env.maxPoolBuilders) || 50;
 
@@ -19,8 +23,9 @@ const maxPoolBuilders = parseInt(process.env.maxPoolBuilders) || 50;
     const builders = [];
     const builderCommand = utilities.getPoolDeployerCommand()
     while (builders.length < currentNeed){
-      builders.push(builderCommand);
+      builders.push(execProm(builderCommand));
     }
+    logger.debug(`starting ${builders.length} of${currentNeed} builders for poolQueue`);
     await Promise.all(builders);
     await prepareAll();
   }
