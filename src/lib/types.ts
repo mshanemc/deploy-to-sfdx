e1719519e2c6345b1b7433c8eabb35559e3f7c13
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-12 14:06:04 -0500

    add semi close

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -89,7 +89,7 @@ export interface clientDataStructure {
   errors: clientError[];
   commandResults: clientResult[];
 
-  poolLines?: lineParserResult
+  poolLines?: lineParserResult;
 }
 
 interface clientError {
