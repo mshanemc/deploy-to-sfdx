43b8c68b2f85dcbb0cba542e1c8111a736eaf4f5
mshanemc <shane.mclaughlin@salesforce.com>
2019-06-10 08:50:31 -0500

    test all get routes

--- test/integrationTests/miscServer.test.ts
+++ test/integrationTests/miscServer.test.ts
@@ -38,5 +38,39 @@ describe('misc server tests', () => {
     });
     expect(response.statusCode).toBe(200);
   });
+
+  test('returns the delete confirm page', async () => {
+    const response = await request.get({
+      url: `${testURL}/deleteConfirm`,
+      resolveWithFullResponse: true
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('returns the error  page', async () => {
+    const response = await request.get({
+      url: `${testURL}/error`,
+      resolveWithFullResponse: true
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('returns the deploying page', async () => {
+    const response = await request.get({
+      url: `${testURL}/deploying/deployer/test-123456`,
+      resolveWithFullResponse: true
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('returns the trial deploying page', async () => {
+    const response = await request.get({
+      url: `${testURL}/deploying/trial/test-123455`,
+      resolveWithFullResponse: true
+    });
+    expect(response.statusCode).toBe(200);
+  });
+
+
 });
 
