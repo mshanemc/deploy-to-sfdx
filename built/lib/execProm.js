261a691873fb19a49e41f4d512bbf18317480cff
mshanemc <shane.mclaughlin@salesforce.com>
2019-06-11 08:06:36 -0500

    try to build on heroku

--- built/lib/execProm.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const child_process_1 = require("child_process");
-const util = require("util");
-const execProm = util.promisify(child_process_1.exec);
-exports.execProm = execProm;
-exports.exec = execProm;
--- built/processes/poolConsumer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const poolBuild_1 = require("../lib/poolBuild");
-(async () => {
-    await poolBuild_1.poolBuild();
-    process.exit(0);
-})();
