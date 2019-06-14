70fe63c37f4d19e32e39b02e48b648c4607ceadd
mshanemc <shane.mclaughlin@salesforce.com>
2019-06-14 11:21:05 -0500

    fix GA time tracking on deploys

--- src/lib/timeTracking.ts
+++ src/lib/timeTracking.ts
@@ -2,37 +2,39 @@ import * as logger from 'heroku-logger';
 import { clientDataStructure, deployRequest } from './types';
 
 const timesToGA = (msgJSON: deployRequest, CDS: clientDataStructure) => {
+  
     try {
       msgJSON.visitor
         .timing(
           'time in queue',
           msgJSON.template,
-          CDS.buildStartTime.getTime() - CDS.browserStartTime.getTime()
+          new Date(CDS.buildStartTime).getTime() - new Date(CDS.browserStartTime).getTime()
         )
         .send();
       msgJSON.visitor
         .timing(
           'time to build',
           msgJSON.template,
-          CDS.completeTimestamp.getTime() - CDS.buildStartTime.getTime()
+          new Date(CDS.completeTimestamp).getTime() - new Date(CDS.buildStartTime).getTime()
         )
         .send();
       msgJSON.visitor
         .timing(
           'time until fully deployed',
           msgJSON.template,
-          CDS.completeTimestamp.getTime() - CDS.browserStartTime.getTime()
+          new Date(CDS.completeTimestamp).getTime() - new Date(CDS.browserStartTime).getTime()
         )
         .send();
       msgJSON.visitor
         .timing(
           'time until open button appears',
           msgJSON.template,
-          CDS.openTimestamp.getTime() - CDS.browserStartTime.getTime()
+          new Date(CDS.openTimestamp).getTime() - new Date(CDS.browserStartTime).getTime()
         )
         .send();
     } catch (e) {
       logger.warn('GA timestamps not firing', msgJSON);
+      logger.warn('acutal GA error', e);
     }
   };
 
