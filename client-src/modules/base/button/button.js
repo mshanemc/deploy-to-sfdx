6f65d2b5c335c2562367f569421d222d9e137b5f
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-01 10:35:01 -0500

    slds button

---
+++ client-src/modules/base/button/button.js
@@ -0,0 +1,41 @@
+import { LightningElement, api, track } from 'lwc';
+
+export default class Button extends LightningElement {
+
+    @track styleClass = 'slds-button';
+
+    @api disabled;
+    @api label;
+    @api 
+    get variant() {
+        return this._variant;
+    }
+    set variant(value) {
+        this._variant = value;
+        switch (value) {
+            case 'base': this.styleClass = 'slds-button'; break;
+            case 'neutral': this.styleClass = 'slds-button slds-button_neutral'; break;
+            case 'brand': this.styleClass = 'slds-button slds-button_brand'; break;
+            case 'destructive': this.styleClass = 'slds-button slds-button_destructive'; break;
+            case 'success': this.styleClass = 'slds-button slds-button_success'; break;
+            case 'inverse': this.styleClass = 'slds-button slds-button_inverse'; break;
+            case 'outline-brand': this.styleClass = 'slds-button slds-button_outline-brand'; break;
+            case 'text-destructive': this.styleClass = 'slds-button slds-button_text-destructive'; break;
+        }
+    }
+
+    click(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        this.dispatchEvent( new CustomEvent( 
+            'click', 
+            {
+                detail: {
+                    label: this.label
+                }
+            }
+        ));
+    }
+
+
+}
