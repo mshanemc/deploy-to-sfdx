d22b97fbd45118c76afa5d3b5d8fd197d263ecd4
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-01 12:15:44 -0500

    trial loader uses ws

--- client-src/modules/route/trialLoader/trialLoader.js
+++ client-src/modules/route/trialLoader/trialLoader.js
@@ -4,16 +4,15 @@ import wsSubscribe from '../../messages/wsWire/wsWire';
 export default class TrialLoader extends LightningElement {
   @api deployId;
 
-    @wire(wsSubscribe, { uri: location.href.replace(/^http/, 'ws'), log: true})
-    wiredResults({error, data}) {
-        if (error) {
-            console.error('error from ws subscribe wire', error);
-        } else if (data) {
-            console.log(data);
-            if (data.mainUser && data.mainUser.loginUrl) {
-                window.location.href = data.mainUser.loginUrl;
-            }
-        }
+  @wire(wsSubscribe, { uri: location.href.replace(/^http/, 'ws'), log: true })
+  wiredResults({ error, data }) {
+    if (error) {
+      console.error('error from ws subscribe wire', error);
+    } else if (data) {
+      console.log(data);
+      if (data.mainUser && data.mainUser.loginUrl) {
+        window.location.href = data.mainUser.loginUrl;
+      }
     }
   }
 }
