c265cd937094b2e36dad3eaeb70a6992851b68ec
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-08 15:41:20 -0500

    use the new CDS delete instead of diretly hitting org

--- test/helpers/repoCodeGen.ts
+++ test/helpers/repoCodeGen.ts
@@ -3,14 +3,14 @@ import { testRepos } from '../testRepos';
 
 const folder = 'test/integrationTests/generatedRepos';
 
-( async () => {
+(async () => {
     await fs.emptyDir(folder);
 
     let flatRepos = [];
-    Object.keys(testRepos).forEach( key => {
+    Object.keys(testRepos).forEach(key => {
         flatRepos = [...flatRepos, ...testRepos[key]];
     });
-    
+
     for (const testRepo of flatRepos) {
         const output = `
 import { deployCheck } from './../../helpers/deployCheck';
@@ -26,7 +26,7 @@ import { sfdxTimeout } from './../../helpers/testingUtils';
 import { requestAddToPool, requestBuildPool } from './../../helpers/poolHelpers';
 import { pooledOrgFinder } from './../../../src/lib/pooledOrgFinder'; 
 import { deployRequest } from './../../../src/lib/types';
-import { deleteOrg } from './../../../src/lib/redisNormal';
+import { cdsDelete } from './../../../src/lib/redisNormal';
 
 const tr = {
     username: '${testRepo.username}',
@@ -49,20 +49,19 @@ describe('pool for ${testRepo.username}/${testRepo.repo}', () => {
                 createdTimestamp: new Date(),
                 whitelisted: true
             }
-            const foundInPool = await pooledOrgFinder(req);
+            const foundInPool = await pooledOrgFinder(req, true);
             expect(foundInPool).toBeTruthy();
             
             // delete the org
-            await deleteOrg(foundInPool.mainUser.username);
+            await cdsDelete(foundInPool.deployId);
         }, sfdxTimeout);       
         
 });
 `;
         await fs.writeFile(`${folder}/${testRepo.username}-${testRepo.repo}.test.ts`, output);
-        
+
         if (testRepo.testPool) {
             await fs.writeFile(`${folder}/${testRepo.username}-${testRepo.repo}-pool.test.ts`, output2);
         }
-    };
-
+    }
 })();
