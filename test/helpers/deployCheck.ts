3b8e4a622ed4bc7b5449ebdfa9850e9a487562a4
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-08 18:50:48 -0500

    retries on org deploy fails

--- test/helpers/deployCheck.ts
+++ test/helpers/deployCheck.ts
@@ -1,13 +1,12 @@
 import * as fs from 'fs-extra';
 import * as request from 'request-promise-native';
+import { sleep, retry } from '@lifeomic/attempt';
 
 import { getTestURL } from './../helpers/testingUtils';
-import { sfdxTimeout } from './../helpers/testingUtils';
 import { cdsDelete } from './../../src/lib/redisNormal';
 import { CDS } from './../../src/lib/CDS';
 
-import { execProm } from '../../src/lib/execProm';
-import { sleep } from '@lifeomic/attempt';
+const retryOptions = { maxAttempts: 3 };
 
 const deployCheck = async (user: string, repo: string) => {
     await fs.ensureDir('tmp');
@@ -15,41 +14,40 @@ const deployCheck = async (user: string, repo: string) => {
     const baseUrl = getTestURL();
     const url = `https://github.com/${user}/${repo}`;
 
-    // fetch get the launch page
+    await retry(async context => {
+        // get the launch page and follow the path
 
-    const startResult = await request({
-        url: `${baseUrl}/launch?template=${url}`,
-        resolveWithFullResponse: true
-    });
-
-    // console.log(startResult.req.path);
-    const deployId = startResult.req.path.replace('/deploying/deployer/', '');
-    // console.log(deployId);
-    const resultsUrl = `${baseUrl}/results/${deployId}`;
-    console.log(resultsUrl);
-    // expect the deploying page redirect.  Get its url so we can check its results by id
-
-    let status = new CDS({
-        deployId
-    });
-    // fetch get the /results url until it's complete
-
-    while (!status.complete) {
-        status = await request({
-            uri: resultsUrl,
-            json: true
+        const startResult = await request({
+            url: `${baseUrl}/launch?template=${url}`,
+            resolveWithFullResponse: true
         });
-        await sleep(1000);
-    }
 
-    expect(status.complete).toBe(true);
-    expect(status.errors).toHaveLength(0);
-    expect(status.mainUser.username).toBeTruthy();
-    expect(status.orgId).toBeTruthy();
-    expect(status.expirationDate).toBeTruthy();
+        // expect the deploying page redirect.  Get its url so we can check its results by id
+        const deployId = startResult.req.path.replace('/deploying/deployer/', '');
+        const resultsUrl = `${baseUrl}/results/${deployId}`;
 
-    await cdsDelete(deployId);
-    return status;
+        let status = new CDS({
+            deployId
+        });
+
+        // fetch get the /results url until it's complete
+        while (!status.complete) {
+            status = await request({
+                uri: resultsUrl,
+                json: true
+            });
+            await sleep(1000);
+        }
+
+        await cdsDelete(deployId);
+
+        expect(status.complete).toBe(true);
+        expect(status.errors).toHaveLength(0);
+        expect(status.mainUser.username).toBeTruthy();
+        expect(status.orgId).toBeTruthy();
+        expect(status.expirationDate).toBeTruthy();
+        return status;
+    }, retryOptions);
 };
 
 export { deployCheck };
