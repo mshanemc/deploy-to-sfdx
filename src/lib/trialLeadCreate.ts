b96cda224075a3b52e055d2e0cf674527241d7d6
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-15 17:07:48 -0500

    don't log lead create errors

--- src/lib/trialLeadCreate.ts
+++ src/lib/trialLeadCreate.ts
@@ -12,49 +12,47 @@ const resultPage = '/conf.html';
 const requestHost = 'www.salesforce.com';
 
 const emitLead = incoming => {
-	emitter.emit('lead', incoming);
+    emitter.emit('lead', incoming);
 };
 
 emitter.on('lead', async incoming => {
-	if (process.env.sfdcLeadCaptureServlet){
-		await leadCreate(incoming);
-	}
+    if (process.env.sfdcLeadCaptureServlet) {
+        await leadCreate(incoming);
+    }
 });
 
-const leadCreate = async function (incoming) {
+const leadCreate = async function(incoming) {
+    const formPostBody = {
+        UserFirstName: incoming.UserFirstName,
+        UserLastName: incoming.UserLastName,
+        CompanyName: incoming.CompanyName,
+        UserTitle: incoming.UserTitle,
+        UserEmail: incoming.UserEmail,
+        UserPhone: incoming.UserPhone,
+        CompanyState: incoming.CompanyState,
+        CompanyPostalCode: incoming.CompanyPostalCode,
+        CompanyEmployees: incoming.CompanyEmployees,
+        CompanyCountry: incoming.CompanyCountry,
+        mcloudFormName: incoming.mcloudFormName,
+        'Lead.LeadSource': incoming['Lead.LeadSource'],
+        FormCampaignId: incoming.FormCampaignId,
+        DriverCampaignId: incoming.DriverCampaignId,
+        requestPage,
+        resultPage,
+        requestHost
+    };
 
-	const formPostBody = {
-		UserFirstName: incoming.UserFirstName,
-		UserLastName: incoming.UserLastName,
-		CompanyName: incoming.CompanyName,
-		UserTitle: incoming.UserTitle,
-		UserEmail: incoming.UserEmail,
-		UserPhone: incoming.UserPhone,
-		CompanyState: incoming.CompanyState,
-		CompanyPostalCode: incoming.CompanyPostalCode,
-		CompanyEmployees: incoming.CompanyEmployees,
-		CompanyCountry: incoming.CompanyCountry,
-		mcloudFormName: incoming.mcloudFormName,
-		'Lead.LeadSource': incoming['Lead.LeadSource'],
-		FormCampaignId: incoming.FormCampaignId,
-		DriverCampaignId: incoming.DriverCampaignId,
-		requestPage,
-		resultPage,
-		requestHost
-	};
-
-	try {
-		await request({
-			url: sfdcLeadCaptureServlet,
-			method: 'POST',
-			strictSSL : false,
-			headers: { 'content-type': 'application/x-www-form-urlencoded' },
-			form: formPostBody
-		});
-	} catch (e){
-		logger.error('error in trialLeadCreate');
-	}
-
+    try {
+        await request({
+            url: sfdcLeadCaptureServlet,
+            method: 'POST',
+            strictSSL: false,
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            form: formPostBody
+        });
+    } catch (e) {
+        // logger.error('error in trialLeadCreate');
+    }
 };
 
 export { emitLead };
