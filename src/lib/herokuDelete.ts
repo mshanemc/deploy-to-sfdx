ef08c09d99bc419e84f8770774669a82a94693c7
mshanemc <shane.mclaughlin@salesforce.com>
2019-07-19 00:47:48 -0700

    try catch on heroku delete

--- src/lib/herokuDelete.ts
+++ src/lib/herokuDelete.ts
@@ -1,18 +1,23 @@
 import * as request from 'request-promise-native';
+import * as logger from 'heroku-logger';
 
-const herokuDelete = async (appName : string) => {
+const herokuDelete = async (appName: string) => {
     const headers = {
         Accept: 'application/vnd.heroku+json; version=3',
         Authorization: `Bearer ${process.env.HEROKU_API_KEY}`
     };
 
-    const deleteResult = await request.delete({
-        url: `https://api.heroku.com/apps/${appName}`,
-        headers,
-        json: true
-    });
+    try {
+        const deleteResult = await request.delete({
+            url: `https://api.heroku.com/apps/${appName}`,
+            headers,
+            json: true
+        });
 
-    return deleteResult;
-}
+        return deleteResult;
+    } catch (e) {
+        logger.error(`error deleting heroku app ${appName}`, e);
+    }
+};
 
-export { herokuDelete }
+export { herokuDelete };
