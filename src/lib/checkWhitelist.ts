961d45fbc24cd5e0f4d2bafbde928f0396ff8ddd
mshanemc <shane.mclaughlin@salesforce.com>
2019-08-15 00:49:47 -0500

    whitelisting and testin

--- src/lib/checkWhitelist.ts
+++ src/lib/checkWhitelist.ts
@@ -5,15 +5,21 @@ const checkWhitelist = (ghuser: string, ghrepo: string) => {
     const whitelist1 = process.env.GITHUB_USERNAME_WHITELIST; // comma separated list of username
     const whitelist2 = process.env.GITHUB_REPO_WHITELIST; // comma separated list of username/repo
 
+    // logger.warn(`whitelist is ${whitelist1}`);
+
     if (!whitelist1 && !whitelist2) {
+        // logger.debug('no whitelists, returning early');
+
         return false;
     }
 
     if (whitelist1) {
         for (const username of whitelist1.split(',')) {
             if (username.trim() === ghuser) {
+                // logger.debug(`matched ${username} and ${ghuser}`);
                 return true;
             }
+            // logger.debug(` not matched ${username} and ${ghuser}`);
         }
     }
 
