d978310ed578ecff4ef4e3f1acbcc243c4b6455b
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-07 14:45:38 -0600

    posts for just fine

--- src/server/__tests__/integrationTests/trialsignup.test.ts
+++ src/server/__tests__/integrationTests/trialsignup.test.ts
@@ -1,60 +1,76 @@
-import * as puppeteer from 'puppeteer';
+// import * as puppeteer from 'puppeteer';
 
-import { sfdxTimeout , getTestURL } from '../helpers/testingUtils';
+// import { sfdxTimeout, getTestURL } from '../helpers/testingUtils';
+import { leadCreate } from '../../lib/leadSupport';
+import { sample } from '../../__tests__/helpers/sampleLead';
 
-const shouldBeLoaded = 1000;
+// const shouldBeLoaded = 1000;
 
-describe.skip('runs the trial', () => {
-    let browser, page;
-    let baseUrl = getTestURL();
-
-    beforeAll( async () => {
-        browser = await puppeteer.launch({headless:true});
-        page = await browser.newPage(); 
-    });
-
-    test('gets form and completes it successfully', async() => {
-        // const browser = await puppeteer.launch({headless:false});
-        // const page = await browser.newPage();
-        // const baseUrl = getTestURL();
-
-        await page.goto(`${baseUrl}/testform`);
-        expect( await page.url() ).toContain('/testform');        
-
-        await page.waitForSelector('input#UserEmail', { timeout: shouldBeLoaded});
-        await page.waitForSelector('input#UserFirstName', { timeout: shouldBeLoaded});
-        await page.waitForSelector('input#UserLastName', { timeout: shouldBeLoaded});
-        await page.waitForSelector('input#submitButton', { timeout: shouldBeLoaded});
-
-        await page.type('input#UserEmail', 'm.shane.mclaughlin@gmail.com');
-        await page.type('input#UserFirstName', 'shane');
-        await page.type('input#UserLastName', 'mclaughlin');
-    }, sfdxTimeout);
-
-    test('submits form and gets to loading screen, then leaves', async() => {
-        await page.waitForSelector('input#submitButton', { timeout: shouldBeLoaded});
-
-        await Promise.all([
-            page.click('input#submitButton'),
-            page.waitForNavigation()
-        ]);
-
-        const deployingUrl = await page.url();
-        expect(deployingUrl).toContain('/deploying/trial/mshanemc-platformTrial-');
-        await page.waitForSelector('.bluebar', { timeout: shouldBeLoaded});
-        await page.waitForNavigation({timeout:sfdxTimeout});
-    }, sfdxTimeout);
-
-    test('eventually gets to actual trial', async() => {
-
-        await page.waitForSelector('div.cTrialWelcome2');
-
-        const trialUrl = await page.url();
-        expect(trialUrl).toContain('lightning.force');
-
-    }, sfdxTimeout);
-
-    afterAll( async() => {
-        browser.close();
+describe('tests org62 signup stuff', () => {
+    it('calls servlet', async () => {
+        const response = await leadCreate(sample);
+        console.log(response);
     });
 });
+
+// describe.skip('runs the trial', () => {
+//     let browser, page;
+//     let baseUrl = getTestURL();
+
+//     beforeAll(async () => {
+//         browser = await puppeteer.launch({ headless: true });
+//         page = await browser.newPage();
+//     });
+
+//     test(
+//         'gets form and completes it successfully',
+//         async () => {
+//             // const browser = await puppeteer.launch({headless:false});
+//             // const page = await browser.newPage();
+//             // const baseUrl = getTestURL();
+
+//             await page.goto(`${baseUrl}/testform`);
+//             expect(await page.url()).toContain('/testform');
+
+//             await page.waitForSelector('input#UserEmail', { timeout: shouldBeLoaded });
+//             await page.waitForSelector('input#UserFirstName', { timeout: shouldBeLoaded });
+//             await page.waitForSelector('input#UserLastName', { timeout: shouldBeLoaded });
+//             await page.waitForSelector('input#submitButton', { timeout: shouldBeLoaded });
+
+//             await page.type('input#UserEmail', 'm.shane.mclaughlin@gmail.com');
+//             await page.type('input#UserFirstName', 'shane');
+//             await page.type('input#UserLastName', 'mclaughlin');
+//         },
+//         sfdxTimeout
+//     );
+
+//     test(
+//         'submits form and gets to loading screen, then leaves',
+//         async () => {
+//             await page.waitForSelector('input#submitButton', { timeout: shouldBeLoaded });
+
+//             await Promise.all([page.click('input#submitButton'), page.waitForNavigation()]);
+
+//             const deployingUrl = await page.url();
+//             expect(deployingUrl).toContain('/deploying/trial/mshanemc-platformTrial-');
+//             await page.waitForSelector('.bluebar', { timeout: shouldBeLoaded });
+//             await page.waitForNavigation({ timeout: sfdxTimeout });
+//         },
+//         sfdxTimeout
+//     );
+
+//     test(
+//         'eventually gets to actual trial',
+//         async () => {
+//             await page.waitForSelector('div.cTrialWelcome2');
+
+//             const trialUrl = await page.url();
+//             expect(trialUrl).toContain('lightning.force');
+//         },
+//         sfdxTimeout
+//     );
+
+//     afterAll(async () => {
+//         browser.close();
+//     });
+// });
--- src/server/processes/trialLeadCreate.ts
+++ src/server/processes/trialLeadCreate.ts
@@ -1,44 +1,8 @@
-// asynchronously posts the lead form back to org62
-import querystring from 'querystring';
-import axios from 'axios';
 import logger from 'heroku-logger';
 
 import { getLead, getLeadQueueSize, putFailedLead } from '../lib/redisNormal';
 
-const sfdcLeadCaptureServlet = process.env.sfdcLeadCaptureServlet;
-const requestPage = '/form.html';
-const resultPage = '/conf.html';
-const requestHost = 'www.salesforce.com';
-
-const leadCreate = async function(incoming) {
-    const formPostBody = {
-        UserFirstName: incoming.UserFirstName,
-        UserLastName: incoming.UserLastName,
-        CompanyName: incoming.CompanyName,
-        UserTitle: incoming.UserTitle,
-        UserEmail: incoming.UserEmail,
-        UserPhone: incoming.UserPhone,
-        CompanyState: incoming.CompanyState,
-        CompanyPostalCode: incoming.CompanyPostalCode,
-        CompanyEmployees: incoming.CompanyEmployees,
-        CompanyCountry: incoming.CompanyCountry,
-        mcloudFormName: incoming.mcloudFormName,
-        'Lead.LeadSource': incoming['Lead.LeadSource'],
-        FormCampaignId: incoming.FormCampaignId,
-        DriverCampaignId: incoming.DriverCampaignId,
-        requestPage,
-        resultPage,
-        requestHost
-    };
-
-    const response = await axios.post(sfdcLeadCaptureServlet, {
-        // strictSSL: false,
-        headers: { 'content-type': 'application/x-www-form-urlencoded' },
-        data: querystring.stringify(formPostBody)
-        // simple: true
-    });
-    logger.debug(JSON.stringify(response));
-};
+import { leadCreate } from '../lib/leadSupport';
 
 (async () => {
     logger.debug('Lead queue consumer is up');
