188000ff443453dbd5cb7c2062f986e38cd04e9b
mshanemc <shane.mclaughlin@salesforce.com>
2019-12-15 17:20:46 -0600

    organize env stuff

--- src/server/lib/processWrapper.ts
+++ src/server/lib/processWrapper.ts
@@ -1,14 +1,12 @@
 const processWrapper = {
     PORT: process.env.PORT || 8443,
-    REDIS_URL: process.env.REDIS_URL,
 
-    SFDX_PRERELEASE: process.env.SFDX_PRERELEASE,
-
-    // hub authentication credentials
+    // devhub authentication and plugin config
     LOCAL_ONLY_KEY_PATH: process.env.LOCAL_ONLY_KEY_PATH,
     JWTKEY: process.env.JWTKEY,
     CONSUMERKEY: process.env.CONSUMERKEY,
     HUB_USERNAME: process.env.HUB_USERNAME,
+    SFDX_PRERELEASE: process.env.SFDX_PRERELEASE,
 
     // bring your own hub credentials
     BYOO_SECRET: process.env.BYOO_SECRET,
@@ -19,18 +17,21 @@ const processWrapper = {
     GITHUB_USERNAME_WHITELIST: process.env.GITHUB_USERNAME_WHITELIST,
     GITHUB_REPO_WHITELIST: process.env.GITHUB_REPO_WHITELIST,
 
-    // google analytics
-    UA_ID: process.env.UA_ID,
-
-    sfdcLeadCaptureServlet: process.env.sfdcLeadCaptureServlet,
-
+    // org pool stuff
     maxPoolBuilders: parseInt(process.env.maxPoolBuilders) || 50,
     POOLCONFIG_URL: process.env.POOLCONFIG_URL,
 
     // heroku integration stuff
     HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
+    REDIS_URL: process.env.REDIS_URL,
+
     HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    DYNO_TIME_LIMIT: parseInt(process.env.DYNO_TIME_LIMIT) || 30
+    DYNO_TIME_LIMIT: parseInt(process.env.DYNO_TIME_LIMIT) || 30,
+
+    // google analytics
+    UA_ID: process.env.UA_ID,
+
+    sfdcLeadCaptureServlet: process.env.sfdcLeadCaptureServlet
 };
 
 export { processWrapper };
