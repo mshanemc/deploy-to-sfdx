9adcb5d0b33150cabc442fc160a1296fb4bb3e04
mshanemc <shane.mclaughlin@salesforce.com>
2020-01-14 15:09:54 -0600

    has defaults

--- src/server/lib/multirepo/mergeProjectJSONs.ts
+++ src/server/lib/multirepo/mergeProjectJSONs.ts
@@ -9,12 +9,13 @@ const MergeProjectJSONs = ({ projectJSONs, localFilePaths }): ProjectJSON => {
     // put the files in {path: file} format
     const mappedFiles = projectJSONs.reduce((resultingObject, file, index) => ({ ...resultingObject, [localFilePaths[index]]: file }), {});
 
-    // enhancement 0: no defaults since they might collide
+    // enhancement 0: no defaults since they might collide, except 1st one because there has to be one
     for (const path in mappedFiles) {
         for (const dir of mappedFiles[path].packageDirectories) {
             output.packageDirectories.push({ path: `${path}/${dir.path}` });
         }
     }
+    output.packageDirectories[0].default = true;
 
     // enhancement 1: max api version if one exists
     const maxApiVersion = Math.max(
