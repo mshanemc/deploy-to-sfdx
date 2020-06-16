219d6bfc24daf4e7bedf02df1a1076a0fac599e8
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-16 12:02:32 -0500

    use shared base components

--- lwc-services.config.js
+++ lwc-services.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  localModulesDirs: [ 'node_modules' ],
-  sourceDir: './src/client',
-  resources: [ { from: 'src/client/resources', to: 'dist/resources' } ],
-  devServer: {
-    open: false,
-    proxy: { '/': 'http://localhost:8443' },
-    stats: 'errors-only'
-  },
-  server: {
-    proxy: { '/': 'http://localhost:8443' },
-    customConfig: './lib/server/web.js'
-  }
-}
+    localModulesDirs: ['./node_modules/@mshanemc/lwc-oss-base/src/modules'],
+    sourceDir: './src/client',
+    resources: [{ from: 'src/client/resources', to: 'dist/resources' }]
+};
--- src/client/modules/base/card/card.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Card extends LightningElement {}
