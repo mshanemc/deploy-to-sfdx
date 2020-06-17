7a5b615b929088b4b07091d71d8be5afad4fbfe7
mshanemc <shane.mclaughlin@salesforce.com>
2020-06-17 10:22:17 -0500

    we're going to need a bigger buffer

--- src/server/lib/execProm.ts
+++ src/server/lib/execProm.ts
@@ -4,20 +4,21 @@ import stripColor from 'strip-color';
 import * as util from 'util';
 
 const execProm = util.promisify(exec);
+const maxBuffer = 1024 * 3000;
 
 // tslint:disable-next-line: no-any
-const exec2JSON = async (cmd: string, options = {}): Promise<any> => {
+const exec2JSON = async (cmd: string, options = { maxBuffer }): Promise<any> => {
     try {
         const results = await execProm(cmd, options);
         return JSON.parse(stripColor(results.stdout));
     } catch (err) {
-        // console.log(err);
+        console.log(err);
         return JSON.parse(stripColor(err.stdout));
     }
 };
 
 // tslint:disable-next-line: no-any
-const exec2String = async (cmd: string, options = {}): Promise<any> => {
+const exec2String = async (cmd: string, options = { maxBuffer }): Promise<any> => {
     try {
         const results = await execProm(cmd, options);
         return results.stdout;
